2eb197740d18dfbe8b39eae5849eb50c135a98af
Andrei <126566419+Xeotheosis@users.noreply.github.com>
2024-07-17 18:55:33 +0300

    Add files via upload

---
+++ WebAppNet472/Scripts/jscript.js
@@ -0,0 +1,16 @@
+﻿function formatNumber(input) {
+    // Elimină caracterele care nu sunt cifre sau punct
+    input.value = input.value.replace(/[^\d.]/g, '');
+
+    // Împarte valoarea în două părți: partea întreagă și partea zecimală
+    var parts = input.value.split('.');
+
+    // Asigură că nu sunt mai mult de două zecimale
+    if (parts[1]) {
+        parts[1] = parts[1].substring(0, 2);
+    }
+
+    // Reunim părțile pentru a obține valoarea formatată
+    input.value = parts.join('.');
+}
+
